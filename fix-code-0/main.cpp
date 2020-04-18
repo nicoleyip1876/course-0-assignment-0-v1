3236e3e3c8d7960b936584b2f15d8fdd2576a19b
nicoleyip1876 <17805096+nicoleyip1876@users.noreply.github.com>
2020-04-18 18:14:44 +0000

    Updating all questions

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -4,6 +4,5 @@ using namespace std;
 
 int main() {
   cout << "Hello "; 
-  cout << "World!"; 
-  cout << endl;
+  cout << "World!" << endl;
 }
--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -3,10 +3,7 @@
 using namespace std;
 
 int main() {
-  cout << "I like learning new things like coding!";
-  cout << endl;
-  cout << "I love eating at restaurants - ramen, burgers, chinese food, sushi, thai, indian";
-  cout << endl;
-  cout << "I dance badly";
-  cout << endl;
+  cout << "I like learning new things like coding!" << endl;
+  cout << "I love eating at restaurants - ramen, burgers, chinese food, sushi, thai, indian" << endl;
+  cout << "I dance badly" << endl;
 }
